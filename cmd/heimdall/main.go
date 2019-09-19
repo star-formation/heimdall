ce213a80ecdb5b55c7b69c95a94b06eb4d657e8b
Gustav Simonsson <gustav@argonavis.io>
2019-09-19 14:19:14 -0400

    wire up tesseract dev scene with WebSocket server

--- cmd/heimdall/main.go
+++ cmd/heimdall/main.go
@@ -1,29 +1,30 @@
 /*  Copyright 2019 The heimdall Authors
-	
+
     This file is part of heimdall.
-    
+
     heimdall is free software: you can redistribute it and/or modify
     it under the terms of the GNU Affero General Public License as
     published by the Free Software Foundation, either version 3 of the
     License, or (at your option) any later version.
-	
+
     heimdall is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU Affero General Public License for more details.
-	
+
     You should have received a copy of the GNU Affero General Public License
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 package main
 
 import (
-  "os"
+	"os"
 
-  "github.com/ethereum/go-ethereum/log"
-  "github.com/urfave/cli"
-  
-  "github.com/star-formation/heimdall/websocket"
+	"github.com/ethereum/go-ethereum/log"
+	"github.com/urfave/cli"
+
+	"github.com/star-formation/heimdall/websocket"
+	"github.com/star-formation/tesseract"
 )
 
 func init() {
@@ -35,18 +36,21 @@ func init() {
 }
 
 func main() {
-  app := cli.NewApp()
-  app.Name = "heimdall"
-  app.Version = "0.0.1"
-  app.Usage = "The guardian of the gods.  Heimdall will blow a horn, called the Gjallarhorn, if Asgard is in danger."
-  app.Action = func(c *cli.Context) error {
-	log.Info("heimdall starting..")
-	websocket.Debug()
-    return nil
-  }
-  
-  err := app.Run(os.Args)
-  if err != nil {
-    log.Error("app.Run:", "err", err)
-  }
+	app := cli.NewApp()
+	app.Name = "heimdall"
+	app.Version = "0.0.1"
+	app.Usage = "The guardian of the gods.  Heimdall will blow a horn, called the Gjallarhorn, if Asgard is in danger."
+	app.Action = func(c *cli.Context) error {
+		log.Info("heimdall starting..")
+		// Setup Dev Scene with a few objects
+		tesseract.DevScene1()
+		//websocket.Start(tesseract.ProtocolHandler)
+		websocket.Start(func([]byte) ([]byte, error) { return nil, nil })
+		return nil
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Error("app.Run:", "err", err)
+	}
 }
--- websocket/ws_server.go
+++ websocket/ws_server.go
@@ -1,27 +1,29 @@
 /*  Copyright 2019 The heimdall Authors
-	
+
     This file is part of heimdall.
-    
+
     heimdall is free software: you can redistribute it and/or modify
     it under the terms of the GNU Affero General Public License as
     published by the Free Software Foundation, either version 3 of the
     License, or (at your option) any later version.
-	
+
     heimdall is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU Affero General Public License for more details.
-	
+
     You should have received a copy of the GNU Affero General Public License
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 package websocket
 
 import (
-    "net/http"
+	"net/http"
+	"time"
 
-    "github.com/ethereum/go-ethereum/log"
-    "github.com/gorilla/websocket"
+	"github.com/ethereum/go-ethereum/log"
+	"github.com/gorilla/websocket"
+	"github.com/star-formation/tesseract"
 )
 
 // note: localhost:8081 as addr string works with raw TCP but not with HTTP
@@ -29,34 +31,79 @@ var host = ":8081"
 
 var upgrader = websocket.Upgrader{} // use default options
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	log.Info("func handler: ")
-	c, err := upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		log.Error("upgrade err:", "err", err)
-		return
-	}
-	defer c.Close()
-	for {
-		log.Info("waiting on c.ReadMessage: ")
-		mt, message, err := c.ReadMessage()
-		if err != nil {
-			log.Error("read:", "err", err)
-			break
+func getHTTPHandler(protocolHandler func([]byte) ([]byte, error)) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		log.Info("func handler: ", "r", r)
+		// TODO: secure origin check
+		upgrader.CheckOrigin = func(r *http.Request) bool {
+			return true
 		}
-		log.Info("recv: ", "msg", message)
-		err = c.WriteMessage(mt, message)
+
+		c, err := upgrader.Upgrade(w, r, nil)
 		if err != nil {
-			log.Error("write err:", "err", err)
-			break
+			log.Error("upgrade err:", "err", err)
+			return
+		}
+		defer c.Close()
+
+		sps := websocket.Subprotocols(r)
+		if len(sps) != 1 || sps[0] != "client0.argonavis.io" {
+			log.Error("Unsupported WebSocket Subprotocol: ", "subs", sps)
+			WriteControlClose(c, websocket.CloseProtocolError, "unsupported subprotocol")
+			return
+		}
+
+		for {
+			log.Info("waiting on c.ReadMessage: ")
+			mt, msg, err := c.ReadMessage()
+			if err != nil {
+				log.Error("read:", "err", err)
+				break
+			}
+			log.Info("recv: ", "msg", msg)
+
+			// setup MessageBus sub to engine loop
+			ch := tesseract.S.MB.Subscribe()
+			go func() {
+				for {
+					stateJSON := <-ch
+					err = c.WriteMessage(mt, stateJSON)
+					if err != nil {
+						log.Error("write err:", "err", err)
+						break
+					}
+				}
+			}()
+
+			/*
+				resp, err := protocolHandler(msg)
+				if err != nil {
+					log.Error("protocolHandler", "err", err)
+					WriteControlClose(c, websocket.CloseProtocolError, err.Error())
+				}
+
+				//log.Debug("c.WriteMessage", "resp", resp)
+				err = c.WriteMessage(mt, resp)
+				if err != nil {
+					log.Error("write err:", "err", err)
+					break
+				}
+			*/
 		}
 	}
 }
 
-func Debug() {
+func WriteControlClose(c *websocket.Conn, closeCode int, str string) error {
+	msg := websocket.FormatCloseMessage(closeCode, str)
+	return c.WriteControl(websocket.CloseMessage, msg, time.Now().Add(time.Second*5))
+}
+
+func Start(protocolHandler func([]byte) ([]byte, error)) {
+	handler := getHTTPHandler(protocolHandler)
 	http.HandleFunc("/", handler)
 	err := http.ListenAndServe(host, nil)
 	if err != nil {
 		log.Error("http.ListenAndServe", "err", err)
 	}
+	log.Info("WebSocket Server Started", "host", host)
 }
